d588e289719bd2fcc57198df0b043aef2e8adcdc
Dishant Kapoor <dishantkapoor29@gmail.com>
2023-05-22 23:34:43 +0530

    fixing reqTrial

--- ReqTrial.py
+++ ReqTrial.py
@@ -2,11 +2,27 @@ from kivy.app import App
 from kivy.network.urlrequest import UrlRequest
 
 
-def got_json(req, result):
-    print (req)
-    print (result)
 def failed():
     print("fail")
+    
+    
+    
+    
+def got_json(req, result):
+    for i in result:
+        if "subject" in i.keys():
+            print(i['subject'])
+
+class Data:
+    def __init__(me, result):
+
+
+
+
+
+
+
+
 req = UrlRequest("https://syllabusx-web-server.onrender.com/secondsemesters/CSE", on_success=got_json, on_failure=failed)
 
 class ReqTrial(App):
