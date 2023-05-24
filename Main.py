0fd5fb96821d5270115a610cf286cc9cd8ad40d0
Dishant Kapoor <dishantkapoor29@gmail.com>
2023-05-24 21:17:22 +0530

    still struggling

--- Main.py
+++ Main.py
@@ -1,8 +1,21 @@
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.animation import Animation
+
+
+class AL(AnchorLayout):
+    def animate_it(self, widget, *arg):
+        animate = Animation(
+            background_color= (0,0,1,1)
+        )
 
 class MainKv(App):
+    Window.size=(50, 80)
     pass
+
+
 
 MainKv().run()
